4b709708874d2bb7cfdda8c836284f4ee3323ea8
Nicholas Chera <nachera.dev@gmail.com>
2019-02-23 13:20:20 -0800

    Implement adder

---
+++ integers/adder_test.go
@@ -0,0 +1,12 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 4)
+	expected := 6
+
+	if sum != expected {
+		t.Errorf("got '%d', expected '%d'", sum, expected)
+	}
+}
